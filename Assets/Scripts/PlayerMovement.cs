b8139b26c51ba6ca166edc3c35945a1eec2097b9
Juan Carlos Gómez Abundis <juancg4@live.com>
2020-04-21 13:26:13 -0600

    Script playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rgb2d;
+
+    void Start()
+    {
+        rgb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rgb2d.AddForce(movement* speed * Time.fixedDeltaTime);
+    }
+}
