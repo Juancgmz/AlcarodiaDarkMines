a8a573996d056e2a1a5bd35cc600d2392675138f
a00821930 <a00821930@itesm.mx>
2020-05-18 00:14:52 -0500

    Animacion a movimiento, nuevo mapa y personajes full HD, requiere reañadir los objetos de colisiones, ai y el de UI, cuyos assets siguen alli intactos

---
+++ Assets/Scripts/OscarMov.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class OscarMov : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+        
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
