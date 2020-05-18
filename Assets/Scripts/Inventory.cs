7d8623765652160a3734e8a51acf84129de36872
a00821930 <a00821930@itesm.mx>
2020-05-17 19:02:49 -0500

    Varios updates, no pude arreglar el Game Over

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int objectSlot1 = 0; //Futuro objeto 1
+    public int objectSlot2 = 0; //Futuro objeto 2
+    public int objectSlot3 = 0; //Futuro objeto 3
+    [Serializable] 
+        private int blood = 0;
+        private int bones = 0;
+        private int leathers = 0;
+        private int resources = 0; 
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int HP = 100;
+    public int speed = 1;
+    public string Fname = "John", Sname = "Bravo", Mname = "Sins", Lname = "Depp", name = "Jonny";
+}
